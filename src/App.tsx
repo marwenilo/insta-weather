4785f26ddc4ba6cedd5b5d0d8fe336372a6f021e
marwenilo <marwenmagrii@gmail.com>
2022-11-28 10:11:27 +0100

    add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { ToastContainer, toast } from "material-react-toastify";
+import { selectWeatherForecasts } from "store/slices/weatherSlice";
+import { useAppSelector } from "hooks";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "components/ErrorFallback/ErrorFallback";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
@@ -5,26 +10,24 @@ import Stack from "@mui/material/Stack";
 import TempUnit from "components/TempUnit/TempUnit";
 import Search from "components/Search/Search";
 import WeatherForecast from "components/WeatherForecast/WeatherForecast";
-import { ToastContainer, toast } from "material-react-toastify";
-import { selectWeatherForecasts } from "store/slices/weatherSlice";
-import { useAppSelector } from "hooks";
+
 import "material-react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
+
 function App() {
   const { error } = useAppSelector(selectWeatherForecasts);
-  useEffect(() => {
-    if (error) {
-      toast.error(error, {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-  }, [error]);
+
+  if (error) {
+    toast.error(error, {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
   return (
     <>
       <CssBaseline />
@@ -32,8 +35,10 @@ function App() {
         <Box sx={{ width: "100%" }}>
           <Stack spacing={2} alignItems="center" marginTop={2}>
             <TempUnit />
-            <Search />
-            <WeatherForecast />
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+              <Search />
+              <WeatherForecast />
+            </ErrorBoundary>
           </Stack>
         </Box>
       </Container>
